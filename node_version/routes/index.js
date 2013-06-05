19d03a0d9f696fb7f915e7958697db3c71f19d96
Pratham Agrawal <pagrawl3@yahoo-inc.com>
2013-06-05 15:46:53 -0700

    added new bgs for landing. Enabled custom implementation of parallax

---
+++ node_version/routes/index.js
@@ -0,0 +1,68 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index_compiled = function(req, res){
+	res.render('index_compiled', { title: 'Hello World!' });
+};
+
+exports.resume_compiled = function(req, res){
+	res.render('resume_compiled', { title: 'Hello World!' });
+};
+
+exports.blog_compiled = function(req, res){
+	res.render('blog_compiled', { title: 'Hello World!' });
+};
+
+exports.work_compiled = function(req, res){
+	res.render('blog_compiled', { title: 'Hello World!' });
+};
+
+exports.blog = function(req, res){
+	var pathToTemplate = require('path').resolve(__dirname, '../views') + '/blog.jade';
+	var jade = require('jade');
+	var template = require('fs').readFileSync(pathToTemplate, 'utf8');
+	var jadeFn = jade.compile(template, { filename: pathToTemplate, pretty: true });
+	var renderedTemplate = jadeFn();
+	res.send({
+		"success"	: true,
+		"data"		: renderedTemplate
+	});
+};
+
+exports.work = function(req, res){
+	var pathToTemplate = require('path').resolve(__dirname, '../views') + '/work.jade';
+	var jade = require('jade');
+	var template = require('fs').readFileSync(pathToTemplate, 'utf8');
+	var jadeFn = jade.compile(template, { filename: pathToTemplate, pretty: true });
+	var renderedTemplate = jadeFn();
+	res.send({
+		"success"	: true,
+		"data"		: renderedTemplate
+	});
+};
+
+exports.index = function(req, res){
+	var pathToTemplate = require('path').resolve(__dirname, '../views') + '/index.jade';
+	var jade = require('jade');
+	var template = require('fs').readFileSync(pathToTemplate, 'utf8');
+	var jadeFn = jade.compile(template, { filename: pathToTemplate, pretty: true });
+	var renderedTemplate = jadeFn();
+	res.send({
+		"success"	: true,
+		"data"		: renderedTemplate
+	});
+};
+
+exports.resume = function(req, res){
+	var pathToTemplate = require('path').resolve(__dirname, '../views') + '/resume.jade';
+	var jade = require('jade');
+	var template = require('fs').readFileSync(pathToTemplate, 'utf8');
+	var jadeFn = jade.compile(template, { filename: pathToTemplate, pretty: true });
+	var renderedTemplate = jadeFn();
+	res.send({
+		"success"	: true,
+		"data"		: renderedTemplate
+	});
+};
